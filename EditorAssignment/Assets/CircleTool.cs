76c931be50e8cde36819232a38c73bf75e8ecc65
Kasper Lindmark <45844138+KasperLindm@users.noreply.github.com>
2019-06-25 13:58:38 +0200

    added project

---
+++ EditorAssignment/Assets/CircleTool.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CircleTool : EditorWindow
+{
+    LineRenderer LineRenderer;
+    GameObject GameObject;
+
+    Vector3 Position;
+    Color Color;
+    int Segments;
+    float Radius;
+    float Width;
+
+    [MenuItem("Window/CircleTool")]
+    static void Init()
+    {
+        CircleTool window = (CircleTool)EditorWindow.GetWindow(typeof(CircleTool));
+        window.Show();
+        GetWindow<CircleTool>("CircleTool");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Circle Settings.", EditorStyles.boldLabel);
+
+        GameObject = (GameObject)EditorGUI.ObjectField(new Rect(3, 3, position.width - 6, 20), "Find Dependency", GameObject, typeof(GameObject));
+
+        Position = EditorGUILayout.Vector3Field("Position", Position);
+        Color = EditorGUILayout.ColorField("Color", Color);
+        Segments = EditorGUILayout.IntField("Segments", Segments);
+        Radius = EditorGUILayout.FloatField("Radius", Radius);
+        Width = EditorGUILayout.FloatField("Width", Width);
+
+        if (GUILayout.Button("DrawCircle"))
+        {
+            OnButtonPressed();
+        }
+    }
+
+    void OnButtonPressed()
+    {
+        LineRenderer = GameObject.GetComponent<LineRenderer>();
+
+        LineRenderer.SetPosition(0, Position);
+        LineRenderer.startWidth = Width;
+        LineRenderer.endWidth = Width;
+
+        for (int i = 0; i < Segments; i++)
+        {
+        }
+    }
+}
